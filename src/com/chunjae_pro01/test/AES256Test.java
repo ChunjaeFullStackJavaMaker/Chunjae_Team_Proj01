f02f8b7a294560229c463c274c64398175aff14c
xuanjiong810@gmail.com <xuanjiong810@gmail.com>
2023-08-12 22:30:52 +0900

    암호화 test

--- src/com/chunjae_pro01/test/AES256Test.java
+++ src/com/chunjae_pro01/test/AES256Test.java
@@ -1,6 +1,6 @@
-//package com.chunjae_pro01.test;
+package com.chunjae_pro01.test;
 
-//import javax.crypto.BadPaddingException;
+import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.io.UnsupportedEncodingException;
@@ -10,17 +10,17 @@ import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.InvalidParameterSpecException;
 
-//public class AES256Test {
-//    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException, IllegalBlockSizeException, InvalidKeySpecException, InvalidParameterSpecException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
-//        String oriText = "1111";
-//        String key = "%02x";
+public class AES256Test {
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException, IllegalBlockSizeException, InvalidKeySpecException, InvalidParameterSpecException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
+        String oriText = "1111";
+        String key = "%02x";
 
-//        System.out.println("원래 데이터 : "+oriText);
-//        System.out.println("MD5 : "+AES256.md5(oriText));
-//        System.out.println("SHA256 : "+AES256.sha256(oriText));
-//        String aes = AES256.encryptAES256(oriText, key);
-//        System.out.println("AES256 암호화 : "+AES256.encryptAES256(oriText, key));
-//        System.out.println("AES256 복호화 : "+AES256.decryptAES256(aes, key));
+        System.out.println("원래 데이터 : "+oriText);
+        System.out.println("MD5 : "+AES256.md5(oriText));
+        System.out.println("SHA256 : "+AES256.sha256(oriText));
+        String aes = AES256.encryptAES256(oriText, key);
+        System.out.println("AES256 암호화 : "+AES256.encryptAES256(oriText, key));
+        System.out.println("AES256 복호화 : "+AES256.decryptAES256(aes, key));
 
-//    }
-//}
+    }
+}
